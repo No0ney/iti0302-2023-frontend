9f4ee9559b9c1c967d376f14b90cd04e0a581610
Helen <heatsp@ttu.ee>
2023-11-12 23:04:37 +0200

    Not done yet

---
+++ src/components/TicketSearchBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {reactive} from "vue";
+import Button from "@/components/Button.vue";
+</script>
+
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: none;
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    margin: 2px;
+    border: 2px solid #8dbee1;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  color: red;
+}
+</style>
---
+++ src/views/SearchByIdView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import TicketSearchBar from "@/components/TicketSearchBar.vue";
+import Flight from "@/components/Flight.vue";
+import axios from "axios";
+import {ref} from "vue";
+
+let tickets = ref([]);
+
+const findInformation = (id) => {
+  axios.get("api/ticket/id")
+      .then(response => {
+        tickets.value = response.data;
+      });
+};
+
+</script>
+<template>
+  <main>
+    <h1>Enter your ticket id</h1>
+
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  td {
+    text-align: center;
+  }
+}
+</style>
